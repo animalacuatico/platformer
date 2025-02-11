383a065ff655778bd64cd86093de140800f85e44
aquatic_animal <angelocaceda333@gmail.com>
2025-02-11 20:44:05 +0100

    test
    test

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Coins : MonoBehaviour
 {
     public int coinValue = 1;
-    public AudioClip coinSound;
+    public AudioClip coinSound1, coinSound2;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<platformMovement>())
         {
             GameManager.instance.AddCoin(coinValue);
             Debug.Log("Monedas: " + GameManager.instance.GetCoins());
-            audioManager.instance.PlayAudio(coinSound, "coinSoundGet", false);
+            RandomChance();
+            if (RandomChance())
             Destroy(gameObject);
+            
         }
+    }
+    private int RandomChance()
+    {
+        int rndNumber;
+        rndNumber = Random.Range(0, 2);
+        return rndNumber;
     }
 }
